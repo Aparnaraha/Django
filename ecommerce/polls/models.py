f817e4fb2707a2414c1c148ce237216080302358
Aparnaraha <aparnaraha2912@gmail.com>
2024-06-17 21:49:37 +0530

    Ecommerce Project updated

--- ecommerce/polls/models.py
+++ ecommerce/polls/models.py
@@ -8,7 +8,7 @@ class company(models.Model):
 
 
 class Employee(models.Model):
-  id = models.IntegerField
+  emp_id = models.IntegerField
   name = models.CharField(max_length=100)
   address = models.CharField(max_length=100) 
   salary = models.FloatField
