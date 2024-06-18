dcb722e5379a5a06572bc17dca17215d46829298
Aparnaraha <aparnaraha2912@gmail.com>
2024-06-18 22:39:25 +0530

    Ecommerce Project updated

--- ecommerce/polls/models.py
+++ ecommerce/polls/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class company(models.Model):
   company_id = models.IntegerField
   name = models.CharField(max_length=100)
-  location = models.CharField(max_length=100)
+  location = models.CharField(max_length=50)
 
 
 class Employee(models.Model):
